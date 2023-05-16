b021841699fe8f3a2042c3cccb55b86db7314d7b
Shubham Vibhute <vibhute.shubh@gmail.com>
2023-05-16 11:50:57 +0530

    edit by developer

--- PrimeNumberCheck/PrimeNumberCheck.cpp
+++ PrimeNumberCheck/PrimeNumberCheck.cpp
@@ -6,7 +6,7 @@ int main() {
   int i, n;
   bool is_prime = true;
 
-  cout << "Enter a positive integer: ";
+  cout << "Enter a positive integer: (edit by developer)";
   cin >> n;
 
   // 0 and 1 are not prime numbers
